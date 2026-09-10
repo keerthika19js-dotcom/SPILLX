b95764c6c3caabdf536cc3918359468e3d33ac90
keerthika19js-dotcom <keerthika19.js@gmail.com>
2026-09-10 22:41:21 +0530

    Deploy SPILLX

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/SPILLX/',
+
   server: {
     port: 5173,
     proxy: {
