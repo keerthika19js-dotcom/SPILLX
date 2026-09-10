08d90f5e5933d2982248c4e2d4047f5e73e7c2f9
keerthika19js-dotcom <keerthika19.js@gmail.com>
2026-09-10 22:57:42 +0530

    Restore SAR radar preview on Pages

--- frontend/src/demoData.js
+++ frontend/src/demoData.js
@@ -7,6 +7,8 @@ const gulfSpill = {
   perimeter_km: 288.78,
   confidence: 74.1,
   sensor: 'Sentinel-1 SAR (C-band VV)',
+  raw_preview_base64: 'https://raw.githubusercontent.com/keerthika19js-dotcom/SPILLX/main/sample_data/sar_images/gulf_of_mexico_sentinel1.png',
+  mask_base64: 'https://raw.githubusercontent.com/keerthika19js-dotcom/SPILLX/main/sample_data/sar_images/gulf_of_mexico_sentinel1.png',
 };
 
 const gulfDrift = {
